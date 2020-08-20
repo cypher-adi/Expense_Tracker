ea28e5c63125889eff255dea258b31286d73e498
Cypher <adityasaha444@gmail.com>
2020-08-21 00:07:59 +0530

    button color change

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -6,11 +6,11 @@ const AddTransaction = () => {
       <h3>Add new transaction</h3>
       <form>
         <div className='form-control'>
-          <label for='text'>Text</label>
+          <label htmlFor='text'>Text</label>
           <input type='text' placeholder='Enter text...' />
         </div>
         <div className='form-control'>
-          <label for='amount'>
+          <label htmlFor='amount'>
             Amount <br />
             (negative - expense, positive - income)
           </label>
