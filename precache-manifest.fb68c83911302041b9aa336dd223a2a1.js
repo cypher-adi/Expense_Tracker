a186ed746b582d4221dd180a849153aa2a92c601
Cypher <adityasaha444@gmail.com>
2020-08-21 02:17:46 +0530

    Updates

---
+++ precache-manifest.fb68c83911302041b9aa336dd223a2a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b58feb51f4e088ff9e6a34420b7b19c",
+    "url": "/Expense_Tracker/index.html"
+  },
+  {
+    "revision": "98a214ef210eac7ef44e",
+    "url": "/Expense_Tracker/static/css/main.caef1c1c.chunk.css"
+  },
+  {
+    "revision": "130c33d1fbe59508e2dd",
+    "url": "/Expense_Tracker/static/js/2.cdcf560b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense_Tracker/static/js/2.cdcf560b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a214ef210eac7ef44e",
+    "url": "/Expense_Tracker/static/js/main.46f6097f.chunk.js"
+  },
+  {
+    "revision": "736b0dba7e8fd7119371",
+    "url": "/Expense_Tracker/static/js/runtime-main.0d3c3890.js"
+  },
+  {
+    "revision": "0efa799a62b0f7633d1713580b2d4099",
+    "url": "/Expense_Tracker/static/media/logo.0efa799a.png"
+  }
+]);
