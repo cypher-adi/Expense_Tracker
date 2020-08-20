ac7ac81fa2d5ad77ab2250898535544710e9a4fa
Cypher <adityasaha444@gmail.com>
2020-08-21 00:00:31 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList';
+import AddTransaction from './components/AddTransaction';
 
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </>
   );
 }
 
