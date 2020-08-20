d7d00de0157fc0f261421acb4207c3e4d8c5868c
Cypher <adityasaha444@gmail.com>
2020-08-20 23:47:28 +0530

    style added with header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from './components/Header';
+
 import './App.css';
 
 function App() {
-  return <div>HI</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
